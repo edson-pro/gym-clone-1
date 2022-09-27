c9717fa4c471e0cc4f436bd405c100e6e51d1a06
ntwali edson <ntwaliedson9@gmail.com>
2022-09-27 09:26:11 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Amazon", "sans"],
+      },
+    },
+    backdropBrightness: {
+      15: ".15",
+    },
+    screens: {
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "940px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "640px" },
+      // => @media (max-width: 639px) { ... }
+    },
+    colors: {
+      primary: "#33d172",
+      primaryLight: "#33d172oa",
+      primaryDark: "#5dae5d",
+    },
+  },
+  plugins: [],
+};
